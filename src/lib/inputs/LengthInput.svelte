43bf5370953ac80b46c3c28940f70514a9b0a5ed
Arjun Chib <arjunpatrickchib@gmail.com>
2021-10-24 12:53:01 -0400

    Add nudge controls to LengthInput

--- src/lib/inputs/LengthInput.svelte
+++ src/lib/inputs/LengthInput.svelte
@@ -29,9 +29,30 @@
     window.getSelection().removeAllRanges();
   }
 
+  function nudge(e, dir) {
+    let mag = 1;
+    if (e.getModifierState("Shift")) {
+      mag = e.getModifierState("Control") ? 0.1 : 10;
+    }
+    const text = e.target.textContent;
+    const num = parseFloat(e.target.textContent);
+    const newNum = num + dir * mag;
+    const newNumStr = newNum % 1 === 0 ? newNum : newNum.toFixed(1);
+    e.target.innerText = text.replace(num, newNumStr);
+    node.style[style] = e.target.textContent;
+  }
+
   function keydown(e) {
-    if (e.code === "Enter") {
-      e.target.blur();
+    switch (e.code) {
+      case "Enter":
+        e.target.blur();
+        break;
+      case "ArrowUp":
+        nudge(e, 1);
+        break;
+      case "ArrowDown":
+        nudge(e, -1);
+        break;
     }
   }
 </script>
